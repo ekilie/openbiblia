504294b2294ad267bbbe8b88e954c9b8c7784145
tacheraSasi <tacherasasi@gmail.com>
2026-03-25 20:01:42 +0300

    I want to walk the bibles dir and walk each lang recording the translations list in a json file

--- main.go
+++ main.go
@@ -6,16 +6,26 @@ import (
 	"path/filepath"
 )
 
+// I want to walk the bibles dir and walk each lang recording the translations list in a json file
 func main() {
-	pathToBibleOSISData := "./bibles"
+	pathToBibleOSISData := "./bibles/"
 	err := filepath.WalkDir(pathToBibleOSISData, func(path string, d fs.DirEntry, err error) error {
 		if !d.IsDir() {
 			return filepath.SkipDir
 		}
 
-		println(d.Name())
+		println("===========", d.Name())
+		errr := filepath.WalkDir(d.Name(), func(path string, b fs.DirEntry, err error) error {
+			if b.IsDir() {
+				return filepath.SkipDir
+			}
 
-		return nil
+			println("	", b.Name())
+
+			return nil
+		})
+
+		return errr
 	})
 	if err != nil {
 		log.Fatal(err)
