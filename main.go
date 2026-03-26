958056c03b36154ac191613aad78eae01aadd781
tacheraSasi <tacherasasi@gmail.com>
2026-03-26 10:28:57 +0300

    Add data structures for Bible translations and update comments

--- main.go
+++ main.go
@@ -6,13 +6,36 @@ import (
 	"path/filepath"
 )
 
-// I want to walk the bibles dir and walk each lang recording the translations list in a json file
+// {
+// 	bibles: [
+// 		{"lang": "en", "translations": [
+// 			{"name": "KJV", "path": "kjv.xml"},
+// 			{"name": "NIV", "path": "niv.xml"}
+// 		]},
+// 		{"lang": "sw", "translations": [
+// 			{"name": "KJV", "path": "kjv.xml"},
+// 			{"name": "NIV", "path": "niv.xml"}
+// 		]}
+// 	]
+// }
+
+type  BibleTranslation struct {
+	Lang string `json:"lang"`
+	Translations []Translation `json:"translations"`
+}
+
+type Translation struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+}
+
 func main() {
 	pathToBibleOSISData := "./bibles/"
 	err := filepath.WalkDir(pathToBibleOSISData, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
+
 		if !d.IsDir() {
 			println("--------",d.Name())
 		}else{
